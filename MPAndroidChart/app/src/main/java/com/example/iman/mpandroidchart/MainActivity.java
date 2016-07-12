4c19110ffd5d318fce94870d36dd3965170fc11a
Iman Tabrizian <Tabrizian@outlook.com>
2016-07-12 11:18:50 +0430

    Improves chart sample code

--- MPAndroidChart/app/src/main/java/com/example/iman/mpandroidchart/MainActivity.java
+++ MPAndroidChart/app/src/main/java/com/example/iman/mpandroidchart/MainActivity.java
@@ -1,7 +1,17 @@
 package com.example.iman.mpandroidchart;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.DataSet;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ArrayList<Entry> values = new ArrayList<>();
+        values.add(new Entry(1, 1));
+        LineDataSet set1 = new LineDataSet(values, "DataSet 1");
+
+        // set the line to be drawn like this "- - - - - -"
+        set1.enableDashedLine(10f, 5f, 0f);
+        set1.enableDashedHighlightLine(10f, 5f, 0f);
+        set1.setColor(Color.BLACK);
+        set1.setCircleColor(Color.BLACK);
+        set1.setLineWidth(1f);
+        set1.setCircleRadius(3f);
+        set1.setDrawCircleHole(false);
+        set1.setValueTextSize(9f);
+        set1.setDrawFilled(true);
+
+        LineChart chart = (LineChart) findViewById(R.id.chart);
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
+        dataSets.add(set1); // add the datasets
+
+        // create a data object with the datasets
+        LineData data = new LineData(dataSets);
+
+        chart.setData(data);
+
     }
 }
