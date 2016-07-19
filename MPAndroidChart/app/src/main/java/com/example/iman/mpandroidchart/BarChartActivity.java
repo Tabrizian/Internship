35f62cbea251e7c990ba0eaaf32e64a062874ece
Iman Tabrizian <Tabrizian@outlook.com>
2016-07-19 14:23:46 +0430

    Improves bar activity

---
+++ MPAndroidChart/app/src/main/java/com/example/iman/mpandroidchart/BarChartActivity.java
@@ -0,0 +1,22 @@
+package com.example.iman.mpandroidchart;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.github.mikephil.charting.charts.BarChart;
+
+public class BarChartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bar_chart);
+
+        BarChart barChart = (BarChart) findViewById();
+    }
+
+}
