f4af94a35dceafb8b215bf55c581fde9e4085cc3
Iman Tabrizian <Tabrizian@outlook.com>
2016-08-23 14:17:11 +0430

    Adds AutoCompleteTextView sample code

---
+++ AutoCompleteTextView/app/src/main/java/com/example/iman/autocompletetextview/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.iman.autocompletetextview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, COUNTRIES);
+
+        AutoCompleteTextView textView = (AutoCompleteTextView)
+                findViewById(R.id.autoCompleteTextView);
+        textView.setAdapter(adapter);
+    }
+
+    private static final String[] COUNTRIES = new String[] {
+            "Belgium", "France", "Italy", "Germany", "Spain"
+    };
+}
